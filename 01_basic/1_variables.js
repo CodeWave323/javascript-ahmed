923cf290277365ac477e42af00862ba4c189c760
CodeWave323 <codewave323@gmail.com>
2025-05-10 04:00:50 +0000

    learned alot!!!

---
+++ 01_basic/1_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1234321
+let accountEmail = "codewave323@gmail.com"
+var accountPassword = "12365487"
+acoountCity = "srinagar"
+
+console.table([accountEmail,accountId,accountPassword,acoountCity]);
